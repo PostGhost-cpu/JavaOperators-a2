8c67f173917ecdc5a1715d013fbf9aad1cf2dfc0
Olwethu Mukona <siyaolwethu433@gmail.com>
2026-02-27 14:38:43 +0200

    feat: adds JOptionPane
    Adds:
     - JOptionPane(Object message)
     - showMessageDialog(Component parentComponent, Object message)

---
+++ TUT_Q_v2/src/main/java/com/mycompany/tut_q_v2/DialogMeasure.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tut_q_v2;
+
+/**
+ *
+ * @author lab_services_student
+ */
+import javax.swing.JOptionPane;
+public class DialogMeasure {
+
+    public static void main(String[] args) {
+        //Input
+        int width = Integer.parseInt(JOptionPane.showInputDialog("Enter Width"));
+        int length = Integer.parseInt(JOptionPane.showInputDialog("Enter Length"));
+        
+        //Process
+        int area = length * width;
+        int perimeter = (2 * length) + (2 * width);
+        
+        //Output
+        if(width < 0 && length < 0) {
+            JOptionPane.showMessageDialog(null, "Area: " + area + "m^2" + " & Perimeter: " + perimeter + "m");
+        } else {
+            JOptionPane.showMessageDialog(null, "Input cancelled, empty or less than 0.");
+        }
+    }
+}
