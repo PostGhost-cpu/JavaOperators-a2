a319f52b91fd2624f87b44609ed8c77c7c8e3db1
Olwethu Mukona <siyaolwethu433@gmail.com>
2026-03-05 20:48:28 +0200

    chore: added comments
    Explain the function of sections

--- TUT_Q_v2/src/main/java/com/mycompany/tut_q_v2/DialogMeasure.java
+++ TUT_Q_v2/src/main/java/com/mycompany/tut_q_v2/DialogMeasure.java
@@ -13,6 +13,7 @@ public class DialogMeasure {
 
     public static void main(String[] args) {
         //Input
+        // Show the input dialog and store the result in an integer variable
         int width = Integer.parseInt(JOptionPane.showInputDialog("Enter Width"));
         int length = Integer.parseInt(JOptionPane.showInputDialog("Enter Length"));
         
@@ -21,7 +22,7 @@ public class DialogMeasure {
         int perimeter = (2 * length) + (2 * width);
         
         //Output
-        if(width < 0 && length < 0) {
+        if(width < 0 && length < 0) { // Check if the user entered anything or clicked Cancel/closed the dialog
             JOptionPane.showMessageDialog(null, "Area: " + area + "m^2" + " & Perimeter: " + perimeter + "m");
         } else {
             JOptionPane.showMessageDialog(null, "Input cancelled, empty or less than 0.");
